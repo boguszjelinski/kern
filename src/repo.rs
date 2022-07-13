67e4d2ef73824c913214200f1b48ea73c1b96eca
Jelinski, Bogusz <Bogusz.Jelinski@skatteetaten.no>
2022-07-13 15:02:23 +0200

    route extender

--- src/repo.rs
+++ src/repo.rs
@@ -72,3 +72,54 @@ pub fn find_legs_by_status(client: &mut Client, status: RouteStatus) -> Vec<Leg>
     }
     return ret;
 }
+
+pub fn assignOrder(order_id: i64, leg_id: i32, route_id: i32, eta: i32, calledBy: &str) -> String {   
+    println!("Assigning order_id={} to route_id={}, leg_id={}, routine {}",
+                                            order_id, route_id, leg_id, calledBy);
+    return format!("\
+        UPDATE o SET o.route_id={}, o.leg_id={}, o.cab_id=r.cab_id, o.status=1, o.eta={} \
+        FROM taxi_order o INNER JOIN route r ON r.id={} \
+        WHERE o.id={} AND o.status=0", // it might be cancelled in the meantime, we have to be sure. 
+        route_id, leg_id, eta, route_id, order_id);
+}
+
+pub fn assignOrderFindLeg(order_id: i64, place: i32, route_id: i32, eta: i32, calledBy: &str) -> String {   
+    println!("Assigning order_id={} to route_id={}, leg_id=UNKNOWN, place={}, routine {}",
+                                            order_id, route_id, place, calledBy);
+    return format!("\
+        UPDATE o SET o.route_id={}, o.leg_id=l.id, o.cab_id=r.cab_id, o.status=1, o.eta={} \
+        FROM taxi_order o INNER JOIN route r ON r.id={} \
+        INNER JOIN leg l ON l.route_id={} AND l.place={} \
+        WHERE o.id={} AND o.status=0", // it might be cancelled in the meantime, we have to be sure. 
+        route_id, eta, route_id, route_id, place, order_id);
+}
+
+pub fn create_leg(order_id: i64, from: i32, to: i32, place: i32, status: RouteStatus, dist: i16,
+                  route_id: i32, calledBy: &str) -> String {
+    println!("Adding leg to route: route_id={}, routine {}", route_id, calledBy);
+    return format!("\
+        INSERT INTO leg (from_stand, to_stand, place, distance, status, route_id) VALUES \
+        ({},{},{},{},{},{})", from, to, place, dist, status as u8, route_id);
+}
+
+pub fn updateLegABit(leg_id: i32, to: i32, dist: i16) -> String {
+    println!("Updating existing leg_id={} to={}", leg_id, to);
+    return format!("\
+        UPDATE leg SET to_stand={}, distance={} \
+        WHERE id={}", to, dist, leg_id);
+}
+
+pub fn updateLegABitWithRouteId(route_id: i32, place: i32, to: i32, dist: i16) -> String {
+    // TODO: sjekk in log how many such cases
+    println!("Updating existing leg with route_id={} place={} to={}", route_id, place, to);
+    return format!("\
+        UPDATE leg SET to_stand={}, distance={} \
+        WHERE route_id={} AND place={}", to, dist, route_id, place);
+}
+
+pub fn updatePlacesInLegs(route_id: i32, place: i32) -> String {
+    println!("Updating places in route_id={} starting with place={}", route_id, place);
+    return format!("\
+        UPDATE leg SET place=place+1 \
+        WHERE route_id={} AND place >= {}", route_id, place);
+}
